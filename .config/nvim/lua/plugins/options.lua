727167a8a2596ee2f1f1c4d5f54a1c1725953202
UnknwnDev <45612390+UnknwnDev@users.noreply.github.com>
2026-01-03 15:10:24 -0800

    add nvim conda python support

--- .config/nvim/lua/plugins/options.lua
+++ .config/nvim/lua/plugins/options.lua
@@ -2,3 +2,13 @@ vim.cmd("colorscheme gruvbox") -- set color theme
 
 vim.opt.termguicolors = true --bufferline
 require("bufferline").setup{}
+
+-- -- Custom python provider
+-- local conda_prefix = vim.env.CONDA_PREFIX
+-- if not isempty(conda_prefix) then
+--   vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, conda_prefix .. "/bin/python")
+--   vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, conda_prefix .. "/bin/python")
+-- else
+--   vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, "python")
+--   vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, "python3")
+-- end
